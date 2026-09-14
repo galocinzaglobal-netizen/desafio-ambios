f89c65a8e105bc78f71414a278f46a2a50dc32e1
galocinzaglobal-netizen <galocinza.global@gmail.com>
2026-09-14 16:31:56 -0400

    Add application core

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { AnimatePresence } from "framer-motion";
+import { Background } from "./components/Background";
+import { FinalScreen } from "./components/FinalScreen";
+import { QuestionScreen } from "./components/QuestionScreen";
+import { StartScreen } from "./components/StartScreen";
+import { useGame } from "./hooks/useGame";
+
+export default function App() {
+  const game = useGame();
+  return <div className="app-shell"><div className="game-frame"><Background /><AnimatePresence mode="wait">
+    {game.screen === "start" && <StartScreen key="start" onStart={game.start} />}
+    {(game.screen === "question" || game.screen === "feedback") && <QuestionScreen key="game" question={game.currentQuestion} index={game.currentIndex} score={game.score} selected={game.selectedAnswer} eliminated={game.eliminated} helps={game.helps} hint={game.hint} feedback={game.screen === "feedback"} onAnswer={game.answer} onHelp={game.useHelp} onNext={game.next} />}
+    {game.screen === "final" && <FinalScreen key="final" score={game.score} correctCount={game.correctCount} onRestart={game.start} />}
+  </AnimatePresence></div></div>;
+}
